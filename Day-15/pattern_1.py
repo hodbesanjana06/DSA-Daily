259314bc09d2163a8c71ed2395654a422e12faa7
Sanjana <hodbesanjana22@gmail.com>
2026-04-28 14:52:16 +0530

    Three Patterns Add....

---
+++ Day-15/pattern_1.py
@@ -0,0 +1,26 @@
+'''
+
+        1 
+        1 2 1
+        1 2 3 2 1
+        1 2 3 4 3 2 1 
+        1 2 3 4 5 4 3 2 1
+
+
+'''
+
+n = int(input("enter number : "))
+
+
+
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        print(j , end=" ")
+
+    for j in range(i-1, 0, -1):
+        print(j ,end=" ")
+    
+    print(end="\n")
+
+

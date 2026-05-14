b4edc9a585a844b4718a31603509145f5c0f0e78
Sanjana <hodbesanjana22@gmail.com>
2026-05-14 23:34:08 +0530

    Two Patterns Solve....

---
+++ Day-29/pattern_1.py
@@ -0,0 +1,23 @@
+'''
+Empty Triangle
+
+* 
+* * 
+*   * 
+*     * 
+*       * 
+* * * * * * 
+
+
+'''
+
+n = int(input("Enter Number : "))
+
+for i in range(1, n+1):
+    for j in range(1, i+1):
+        if j == 1 or i == n or i == j:
+            print("*", end=" ")
+        else:
+            print(" ", end=" ")
+
+    print(end="\n")
---
+++ Day-29/pattern_2.py
@@ -0,0 +1,36 @@
+'''
+
+
+         *  
+       *   *  
+     *   *   *  
+   *   *   *   *  
+ *   *   *   *   *  
+   *   *   *   *  
+     *   *   *  
+       *   *  
+         *  
+
+
+'''
+
+n = int(input("Enter Number : "))
+
+for i in range(1, n+1):
+    for j in range(i,n):
+        print(" ", end=" ")
+
+    for j in range(1, i+1):
+        print("*  ", end=" ")
+    print(end="\n")
+   
+for i in range(1, n):
+    for j in range(1, i+1):
+        print(" ", end=' ')
+
+    for j in range(i, n):
+        print("*  ", end=" ")
+
+    print(end="\n")
+
+    
